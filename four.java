316f23d9625d3d43260fdecd4ee6dc7b21bab91c
Fathima Minha <minhasulaikar@gmail.com>
2025-03-15 21:06:28 +0530

    quetion four

---
+++ four.java
@@ -0,0 +1,11 @@
+// 4)print the odd numbers between 1 to 20
+
+public class four{
+    public static void main(String args[]){
+        for(int i=1; i<21; i++){
+            if(i%2==1){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
