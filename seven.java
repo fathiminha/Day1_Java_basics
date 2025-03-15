39809ebe0bec9a3b1cc16358eadfe2235e7ee3d6
Fathima Minha <minhasulaikar@gmail.com>
2025-03-15 19:55:40 +0530

    base class book

--- seven.java
+++ seven.java
@@ -5,15 +5,13 @@ library like Printed books, E books, and Audio books
 You must use inhertance, polymorphism, encapuslation...
  */
 
-abstract class Book{
+class Book{
     private String title;
     private String author;
-    private int year;
 
-    public Book(String title, String author, int year){
+    public Book(String title, String author){
         this.title = title;
         this.author = author;
-        this.year = year;
     }
     public String getTitle(){
         return title;
@@ -21,8 +19,7 @@ abstract class Book{
     public String getAuthor(){
         return author;
     }
-    public int getYear(){
-        return year;
+    public void displayInfo(){
+        System.out.println("Book: "+ title + " by "+ author);
     }
-    public abstract void displayInfo();
 }
