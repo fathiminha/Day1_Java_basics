ad2d440d8b5de54958c81046a2ab5e025451058c
Fathima Minha <minhasulaikar@gmail.com>
2025-03-15 21:00:25 +0530

    quetion two

---
+++ two.java
@@ -0,0 +1,9 @@
+// 2)print numbers from 1 to 10
+
+public class two{
+    public static void main(String args[]){
+        for(int i=1; i<11; i++){
+            System.out.println(i);
+        }
+    }
+}
