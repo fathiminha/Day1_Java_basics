147c73edaff807f0c4e3ffd4f52dd2f82b2ae0d3
Fathima Minha <minhasulaikar@gmail.com>
2025-03-15 21:02:32 +0530

    quetion three

---
+++ three.java
@@ -0,0 +1,9 @@
+// 3)print the numbers in reverse
+
+public class three{
+    public static void main(String args[]){
+        for(int i=10; i>0; i--){
+            System.out.println(i);
+        }
+    }
+}
