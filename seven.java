5584a2b009bccf3b3cdc8230f573d8e677e5ea64
Fathima Minha <minhasulaikar@gmail.com>
2025-03-15 20:15:44 +0530

    quetion seven completed

--- seven.java
+++ seven.java
@@ -23,3 +23,36 @@ class Book{
         System.out.println("Book: "+ title + " by "+ author);
     }
 }
+class PrintedBook extends Book{
+    private int pages;
+
+    public PrintedBook(String title, String author, int pages){
+        super(title, author);
+        this.pages = pages;
+    }
+    public void displayInfo(){
+        System.out.println("Printed book: "+ getTitle() + " by " + getAuthor() + ", PAges: "+ pages );
+    }
+}
+
+class EBook extends Book{
+    private double fileSize;
+
+    public EBook(String title, String author, double fileSize){
+        super(title, author);
+        this.fileSize = fileSize;
+    }
+    public void displayInfo(){
+         System.out.println("E-Book: " + getTitle() + " by " + getAuthor() + ", File Size: " + fileSize + "MB");
+    }
+}
+
+public class Library{
+    public static void main(String[] args){
+        Book book1 = new PrintedBook("Java Basics", "John Doe", 300);
+        Book book2 = new EBook("Python Guide", "Jane Smith", 2.5);
+
+        book1. displayInfo();
+        book2.displayInfo();
+    }
+}
